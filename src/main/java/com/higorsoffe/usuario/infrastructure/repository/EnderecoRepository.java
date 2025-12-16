6b70a70cefff19feb3cab5fc09d6ed232e32b614
Higor <higorsoffe@gmail.com>
2025-12-16 20:27:45 -0300

    adicionadas classes de security

---
+++ src/main/java/com/higorsoffe/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.higorsoffe.usuario.infrastructure.repository;
+
+import com.higorsoffe.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
---
+++ src/main/java/com/higorsoffe/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.higorsoffe.usuario.infrastructure.repository;
+
+import com.higorsoffe.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
